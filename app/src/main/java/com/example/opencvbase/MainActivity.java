bf65bd6e8f0124b3a158acb6d92829a026055c25
fuga-w <fugarian@protonmail.com>
2019-10-30 15:46:09 +0900

    不要なimport文の削除

--- app/src/main/java/com/example/opencvbase/MainActivity.java
+++ app/src/main/java/com/example/opencvbase/MainActivity.java
@@ -1,18 +1,8 @@
 package com.example.opencvbase;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.graphics.Bitmap;
-import android.graphics.Color;
 import android.os.Bundle;
-import android.widget.ImageView;
 
-import org.opencv.android.OpenCVLoader;
-import org.opencv.android.Utils;
-import org.opencv.core.CvType;
-import org.opencv.core.Mat;
-import org.opencv.core.Scalar;
-import org.opencv.core.Size;
 
 public class MainActivity extends AppCompatActivity {
 
